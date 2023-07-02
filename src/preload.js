10952ac52f00cca469e098e8a270fc20a787e59d
WorriedArrow <worriedarrow@outlook.com>
2023-07-01 17:55:24 -0700

    [cd/gh] add npm install to action

---
+++ src/preload.js
@@ -0,0 +1,29 @@
+const { contextBridge, webFrame, ipcRenderer } = require("electron");
+const { app } = require('@electron/remote')
+
+contextBridge.exposeInMainWorld("OpenLoaderNative", {
+    app: {
+        quit() {
+            app.quit();
+        },
+        getZoomFactor() {
+            return webFrame.getZoomFactor() * 100;
+        },
+        setZoomFactor(factor) {
+            webFrame.setZoomFactor(factor / 100);
+        }
+    },
+    ipc: ipcRenderer
+});
+
+(async () => {
+// Load OpenLoader
+const buildInfo = JSON.parse(require('fs').readFileSync(require('path').join(process.resourcesPath, 'build_info.json'), 'utf8'));
+const settings = JSON.parse(require("fs").readFileSync(require('path').join(process.env.DISCORD_USER_DATA_DIR ?? require("path").join(app.getPath('appData'), 'discord' + (buildInfo.releaseChannel === 'stable' ? '' : buildInfo.releaseChannel)), 'settings.json')));
+
+const originalKill = process.kill;
+process.kill = function() {};
+console.log(settings);
+if(settings.olNative.preload ?? true) require(process.env.ORIGINAL_PRELOAD);
+process.kill = originalKill;
+})();
